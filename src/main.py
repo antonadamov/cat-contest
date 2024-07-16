a02ba8c354e1ab056088fe639a8965ce370eca26
Anton Adamov <anton.adamau@gmail.com>
2024-07-15 19:16:24 -0700

    logger configuration was updated

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 def main():
     # Argument parsing
